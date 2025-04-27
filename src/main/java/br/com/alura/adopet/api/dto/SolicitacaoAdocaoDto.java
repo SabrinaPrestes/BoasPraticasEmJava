ee7855776f26ead4e7e2815a21b893e0991a8694
SabrinaPrestes <sabrinaprestes25@gmail.com>
2025-04-27 17:10:17 -0300

    Utilizando DTO na Service

---
+++ src/main/java/br/com/alura/adopet/api/dto/SolicitacaoAdocaoDto.java
@@ -0,0 +1,4 @@
+package br.com.alura.adopet.api.dto;
+
+public record SolicitacaoAdocaoDto(Long idPet, long idTutor, String motivo) {
+}
---
+++ src/main/java/br/com/alura/adopet/api/exception/ValidacaoException.java
@@ -0,0 +1,12 @@
+package br.com.alura.adopet.api.exception;
+
+public class ValidacaoException extends RuntimeException {
+
+    public ValidacaoException(String message) {
+        super(message);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
